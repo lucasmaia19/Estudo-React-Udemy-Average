470986bb436558bb512ea5d91be19ad95b679da6
lucasmaia19 <lucasmaiaalves19@gmail.com>
2021-03-24 17:19:23 -0300

    estudo context-api

---
+++ estudo-react-context-api/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import Alunos from './components/Alunos';
+
+import UserProvider from './contexts/user';
+
+function App() {
+
+  return (
+   <UserProvider>
+      <div>
+        <h1>ESCOLA</h1>
+        <hr/>
+        <Alunos/>
+      </div>
+    </UserProvider> 
+  );
+}
+
+export default App;
